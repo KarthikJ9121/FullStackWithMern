03181dafd5371f93a36febcfa21dc78e544db359
Karthik <karthikjodu9121@gmail.com>
2024-05-18 08:34:21 +0530

    new Files added

--- webApna/restFul/index.js
+++ webApna/restFul/index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const {v4 : uuidv4} = require("uuid");
+const methodOverride = require("method-override")    //to override the method of the html verbs
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -19,17 +21,17 @@ let posts =[
     {
         id : uuidv4(),
         username : "apnaCollege",
-        content : "Welcome to apnacollege"
+        content : "We Create Tons of Content"
     },
     {
         id : uuidv4(),
         username : "striver",
-        content : "Welcome to striver"
+        content : "Largest Ds algo"
     },
     {
         id : uuidv4(),
         username : "rahul",
-        content : "Welcome to rahul"
+        content : "Software Engineer"
     }
 ]
 
@@ -38,14 +40,15 @@ app.get("/posts", (req, res) => {
 });
 
 app.get("/posts/new", (req, res) => {
-   res.render("quoraNewPost.ejs")
+   res.render("quoraNewPost.ejs");
 });
 
 app.post("/posts", (req, res) => {
     let {username, content} = req.body;
-    posts.push({username, content});
+    posts.push({id: uuidv4(), username, content});
+    // console.log(posts);
     res.redirect("/posts")
-    console.log("New Post Created");
+    // console.log("New Post Created");
 });
 
 app.get("/posts/:id", (req, res) => {
@@ -53,6 +56,27 @@ app.get("/posts/:id", (req, res) => {
     let post = posts.find((p) => (id === p.id));
     console.log(post);
     res.render("viewDetails.ejs", {post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    res.redirect(`/posts/${id}`)
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id != p.id);
+    console.log(posts);
+    res.redirect("/posts");
 });
 
 app.listen(port, (req, res) => {
